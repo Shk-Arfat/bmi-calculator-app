76b6838691f7e7c8103cd29e1e48b8410e7e75df
Shk-Arfat <shkarfat92@gmail.com>
2026-02-06 07:40:13 +0530

    Modify app.run to use PORT environment variable
    Update the main execution block to use environment variable for port.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
+
+
+
 
 app = Flask(__name__)
 
@@ -45,5 +49,6 @@ def index():
 
     return render_template('index.html', result=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
